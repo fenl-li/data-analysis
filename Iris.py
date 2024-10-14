b65890a59eff7b2008dea47f339aa6cd3aeb342b
fenl-li <lifenl06@163.com>
2024-10-14 14:25:30 +0800

    3

--- Iris.py
+++ Iris.py
@@ -1,19 +1,14 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 iris_data = pd.read_csv('Iris.csv')
 
 
 print(iris_data.head())
 
-
 mean_values_by_species = iris_data.groupby('Species').mean()
 
-
 print(mean_values_by_species)
-
 
 mean_values_by_species.plot(kind='bar', figsize=(10, 6), color=['blue', 'green', 'red', 'purple'])
 plt.title('Mean Feature Values by Iris Species')
